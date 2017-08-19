95d1063f7675f3b0b530d3c4d74fac7ebe3504fa
Chaitanya <chaitanyabm18@gmail.com>
2017-08-19 23:40:24 +0530

    migration files

--- db/migrate/20170816100746_add_index_confirmation_token_devise.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexConfirmationTokenDevise < ActiveRecord::Migration[5.0]
-  def change
-  	add_index :users, :confirmation_token, unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817172410) do
+ActiveRecord::Schema.define(version: 20170819173545) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "category_title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170817172410) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
+    t.string   "email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -46,8 +47,8 @@ ActiveRecord::Schema.define(version: 20170817172410) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.string   "email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
